8b7d36c49a1965ce8b2fe05373dffbce5137b129
Steve Tarter <steve@oway.io>
2024-03-24 11:40:56 -0500

    Add calculations for the position using a length-indexed line.

--- src/main/java/com/tarterware/roadrunner/controllers/PositionController.java
+++ src/main/java/com/tarterware/roadrunner/controllers/PositionController.java
@@ -1,7 +1,18 @@
 package com.tarterware.roadrunner.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Envelope;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.LineString;
+import org.locationtech.jts.index.SpatialIndex;
+import org.locationtech.jts.index.strtree.STRtree;
+import org.locationtech.jts.linearref.LengthIndexedLine;
+import org.locationtech.jts.linearref.LocationIndexedLine;
+import org.locationtech.proj4j.CoordinateTransform;
+import org.locationtech.proj4j.ProjCoordinate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,8 +24,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tarterware.roadrunner.models.PositionRequest;
 import com.tarterware.roadrunner.models.PositionResponse;
 import com.tarterware.roadrunner.models.mapbox.Directions;
+import com.tarterware.roadrunner.models.mapbox.RouteLeg;
+import com.tarterware.roadrunner.models.mapbox.RouteStep;
 import com.tarterware.roadrunner.services.DirectionsService;
 import com.tarterware.roadrunner.services.GeocodingService;
+import com.tarterware.roadrunner.utilities.TopologyUtilities;
 
 @RestController
 @RequestMapping("/api/position")
@@ -26,6 +40,13 @@ public class PositionController
 	@Autowired
 	GeocodingService geocodingService;
 	
+	private LineString geodeticLineString;
+	private LineString utmLineString;
+	private CoordinateTransform wgs84ToUtmCoordinatetransformer;
+	private CoordinateTransform utmToWgs84Coordinatetransformer;
+	private LengthIndexedLine lengthIndexedLine;
+	private SpatialIndex spatialIndex;
+
 	@PostMapping("/get-position")
 	ResponseEntity<PositionResponse> getPosition(@RequestBody PositionRequest positionRequest)
 	{
@@ -108,6 +129,54 @@ public class PositionController
 			return new ResponseEntity<PositionResponse>(positionResponse, HttpStatus.BAD_REQUEST);
 		}
 		
+		// The position lies on the route, so create the the JTS version.
+		
+		// Create the appropriate coordinate transformers.
+		GeometryFactory geometryFactory = new GeometryFactory();
+		List<Double> startLocation = directions.getWaypoints().get(0).getLocation();
+		ProjCoordinate geodeticCoordinate = new ProjCoordinate(startLocation.get(0), startLocation.get(1), 0.0);
+		Coordinate coord = TopologyUtilities.projCoordToCoord(geodeticCoordinate);
+		wgs84ToUtmCoordinatetransformer = TopologyUtilities.getWgs84ToUtmCoordinateTransformer(coord);
+		utmToWgs84Coordinatetransformer = TopologyUtilities.getUtmToWgs84CoordinateTransformer(coord);
+		
+		List<Coordinate> utmCoordList = new ArrayList<Coordinate>();
+
+		// Now, loop through the legs of the route and create the JTS versions
+		List<RouteLeg> listLegs = directions.getRoutes().get(0).getLegs();
+		for(RouteLeg leg : listLegs)
+		{
+			// Create a version of the leg in UTM coordinates
+			for(RouteStep step : leg.getSteps())
+			{
+				ProjCoordinate projUtmCoord = new ProjCoordinate();
+				for(List<Double> coordinate : step.getGeometry().getCoordinates())
+				{
+					geodeticCoordinate = new ProjCoordinate(coordinate.get(0), coordinate.get(1), 0.0);
+					wgs84ToUtmCoordinatetransformer.transform(geodeticCoordinate, projUtmCoord);
+					utmCoordList.add(TopologyUtilities.projCoordToCoord(projUtmCoord));
+				}
+			}
+		}
+		utmLineString = geometryFactory.createLineString(utmCoordList.toArray(new Coordinate[0]));
+		
+		// Create a spatial index, then stuff the envelope of the UTM line string in it.
+		spatialIndex = new STRtree();
+		Envelope envelope = utmLineString.getEnvelopeInternal();
+		spatialIndex.insert(envelope, new LocationIndexedLine(utmLineString));
+		
+		// Create a length indexed string, and determine what the milepost scale factor should be.
+		lengthIndexedLine = new LengthIndexedLine(utmLineString);
+		
+		// Now that we've created the length indexed line, find the geodetic coordinate to return.
+		Coordinate utmPoint = lengthIndexedLine.extractPoint(metersTravel);
+		ProjCoordinate projUtmPoint = TopologyUtilities.coordToProjCoord(utmPoint);
+		ProjCoordinate projGeoPoint = new ProjCoordinate();
+		utmToWgs84Coordinatetransformer.transform(projUtmPoint, projGeoPoint);
+		
+		positionResponse.setValid(true);
+		positionResponse.setLatitude(projGeoPoint.y);
+		positionResponse.setLongitude(projGeoPoint.x);
+
 		return new ResponseEntity<PositionResponse>(positionResponse, HttpStatus.OK);
 	}
 }
