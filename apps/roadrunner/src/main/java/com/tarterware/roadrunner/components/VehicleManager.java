451d94d55f4bc5d4008c3c8fc126fbc81a81ffcb
Steve Tarter <steve@oway.io>
2024-04-24 08:26:53 -0500

    Add the concept of a Vehicle and add a controller and manager for them.

---
+++ apps/roadrunner/src/main/java/com/tarterware/roadrunner/components/VehicleManager.java
@@ -0,0 +1,114 @@
+package com.tarterware.roadrunner.components;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.tarterware.roadrunner.models.TripPlan;
+import com.tarterware.roadrunner.services.DirectionsService;
+
+@Component
+public class VehicleManager
+{
+    private long msPeriod = 100;
+
+    private Timer timer;
+
+    private Map<UUID, Vehicle> vehicleMap = new HashMap<UUID, Vehicle>();
+	
+	private static final Logger logger = LoggerFactory.getLogger(VehicleManager.class);
+	
+	@Autowired
+	DirectionsService directionsService;
+	
+	public Map<UUID, Vehicle> getVehicleMap()
+	{
+		return vehicleMap;
+	}
+	
+	public Vehicle getVehicle(UUID uuid)
+	{
+		return vehicleMap.get(uuid);
+	}
+	
+    public void startup()
+    {
+        if(timer != null)
+        {
+            throw new IllegalStateException("VehicleManager has already started!");
+        }
+        
+        Random random = new Random();
+        long msDelay = (long) (msPeriod * random.nextDouble());
+        logger.info("Starting VehicleManager with period of " + msPeriod + " ms and delay of " + msDelay + " ms.");;
+        timer = new Timer("VehicleManager Timer");
+        timer.schedule(new UpdateTask(), msDelay, msPeriod);
+    }
+    
+    public void shutdown()
+    {
+        if(timer == null)
+        {
+            throw new IllegalStateException("VehicleManager is not running!");
+        }
+        
+        timer.cancel();
+        timer = null;
+    }
+    
+    public boolean isRunning()
+    {
+        return timer != null;
+    }
+    
+    public long getMsPeriod()
+    {
+        return msPeriod;
+    }
+    
+    public void setMsPeriod(long value)
+    {
+        boolean wasRunning = isRunning();
+        if(wasRunning)
+        {
+            shutdown();
+        }
+        
+        msPeriod = value;
+        
+        if(wasRunning)
+        {
+            startup();
+        }
+    }
+    
+    public UUID createVehicle(TripPlan tripPlan)
+    {
+    	Vehicle vehicle = new Vehicle(directionsService);
+    	vehicle.setTripPlan(tripPlan);
+    	vehicleMap.put(vehicle.getId(), vehicle);
+    	
+    	return vehicle.id;
+    }
+    
+    class UpdateTask extends TimerTask
+    {
+        @Override
+        public void run()
+        {
+        	// Loop through each of the Vehicles and update them.
+        	for(Vehicle vehicle : vehicleMap.values())
+        	{
+        		vehicle.update();
+        	}
+        }
+    }
+}
---
+++ apps/roadrunner/src/main/java/com/tarterware/roadrunner/controllers/VehicleController.java
@@ -0,0 +1,76 @@
+package com.tarterware.roadrunner.controllers;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tarterware.roadrunner.components.Vehicle;
+import com.tarterware.roadrunner.components.VehicleManager;
+import com.tarterware.roadrunner.models.TripPlan;
+import com.tarterware.roadrunner.models.VehicleState;
+
+@RestController
+@RequestMapping("/api/vehicle")
+public class VehicleController
+{
+	@Autowired
+	VehicleManager vehicleManager;
+
+	@PostMapping("/create-new")
+	ResponseEntity<VehicleState> getNewVehicle(@RequestBody TripPlan tripPlan)
+	{
+		UUID vehicleId = vehicleManager.createVehicle(tripPlan);
+		VehicleState vehicleState = createVehicleStateFor(vehicleId);
+		
+		if(vehicleManager.isRunning() == false)
+		{
+			vehicleManager.startup();
+		}
+		return new ResponseEntity<VehicleState>(vehicleState, HttpStatus.OK);
+	}
+	
+	@GetMapping("/get-vehicle-state/{vehicleId}")
+	ResponseEntity<VehicleState> getVehicleStateFor(@PathVariable String vehicleId)
+	{
+		Vehicle vehicle = vehicleManager.getVehicle(UUID.fromString(vehicleId));
+		if(vehicle == null)
+		{
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		VehicleState vehicleState = createVehicleStateFor(vehicle.getId());
+		
+		return new ResponseEntity<VehicleState>(vehicleState, HttpStatus.OK);
+	}
+	
+	
+	private VehicleState createVehicleStateFor(UUID vehicleId)
+	{
+		Vehicle vehicle = vehicleManager.getVehicle(vehicleId);
+		
+		if(vehicle == null)
+		{
+			throw new IllegalArgumentException("Unable to find vehicle with ID " + vehicleId);
+		}
+		
+		VehicleState vehicleState = new VehicleState();
+		vehicleState.setId(vehicle.getId());
+		vehicleState.setDegLatitude(vehicle.getDegLatitude());
+		vehicleState.setDegLongitude(vehicle.getDegLongitude());
+		vehicleState.setMetersOffset(vehicle.getMetersOffset());
+		vehicleState.setMetersPerSecond(vehicle.getMetersPerSecond());
+		vehicleState.setMetersPerSecondDesired(vehicle.getMetersPerSecondDesired());
+		vehicleState.setMssAcceleration(vehicle.getMssAcceleration());
+		vehicleState.setPositionLimited(vehicle.isPositionLimited());
+		vehicleState.setPositionValid(vehicle.isPositionValid());
+		
+		return vehicleState;
+	}
+}
