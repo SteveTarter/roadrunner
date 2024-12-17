3944c1fc91355c0794e9ad33263a539d4c93cb21
Steve Tarter <steve@tarterware.com>
2024-12-17 17:11:32 -0600

    Update to use Spring Boot 3.0

--- apps/roadrunner/src/main/java/com/tarterware/roadrunner/configs/SecurityConfig.java
+++ apps/roadrunner/src/main/java/com/tarterware/roadrunner/configs/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -15,24 +15,25 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter
+public class SecurityConfig 
 {
 
-    @Override
-    protected void configure(HttpSecurity security) throws Exception
-    {
-        security
-            .cors()
-            .and()
-            .csrf().disable()
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.cors()
+			.and()
+			.csrf().disable()
         	.authorizeRequests()
         	// Allow OPTIONS requests without authentication
-        	.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+        	.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
         	// Secure all other endpoints
         	.anyRequest().authenticated()
         	.and()
         	.oauth2ResourceServer().jwt();
-    }
+			
+		return http.build();
+	}
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
