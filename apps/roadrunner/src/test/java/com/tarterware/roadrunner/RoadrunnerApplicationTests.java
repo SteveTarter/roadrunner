c757d7b16a614bfe59d5ec82c2adafd22737c3d9
Steve Tarter <steve@tarterware.com>
2025-01-09 15:14:29 -0600

    Add MockBean to attempt to get past GitHub workflow error with MockBean.

--- apps/roadrunner/src/test/java/com/tarterware/roadrunner/RoadrunnerApplicationTests.java
+++ apps/roadrunner/src/test/java/com/tarterware/roadrunner/RoadrunnerApplicationTests.java
@@ -2,10 +2,16 @@ package com.tarterware.roadrunner;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.tarterware.roadrunner.services.DirectionsService;
 
 @SpringBootTest
 class RoadrunnerApplicationTests
 {
+    @MockBean
+    private DirectionsService directionsService;
+    
     @Test
     void contextLoads()
     {
