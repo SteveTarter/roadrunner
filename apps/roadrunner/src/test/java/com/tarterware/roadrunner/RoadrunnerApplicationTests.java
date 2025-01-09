9aabc24fdb8f9a91d4d6623420f8ab4ecc3eec33
Steve Tarter <steve@tarterware.com>
2025-01-09 15:21:12 -0600

    Exclude SecurityConfig from testing.

--- apps/roadrunner/src/test/java/com/tarterware/roadrunner/RoadrunnerApplicationTests.java
+++ apps/roadrunner/src/test/java/com/tarterware/roadrunner/RoadrunnerApplicationTests.java
@@ -1,12 +1,15 @@
 package com.tarterware.roadrunner;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import com.tarterware.roadrunner.configs.SecurityConfig;
 import com.tarterware.roadrunner.services.DirectionsService;
 
 @SpringBootTest
+@ImportAutoConfiguration(exclude = { SecurityConfig.class })
 class RoadrunnerApplicationTests
 {
     @MockBean
