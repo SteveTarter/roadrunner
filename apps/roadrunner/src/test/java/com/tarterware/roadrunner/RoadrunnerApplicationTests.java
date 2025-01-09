3e359bb30e2ba6322fa400d98e206bf02056f003
Steve Tarter <steve@tarterware.com>
2025-01-09 15:35:37 -0600

    Mock more beans in test application.

--- apps/roadrunner/src/test/java/com/tarterware/roadrunner/RoadrunnerApplicationTests.java
+++ apps/roadrunner/src/test/java/com/tarterware/roadrunner/RoadrunnerApplicationTests.java
@@ -3,9 +3,13 @@ package com.tarterware.roadrunner;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import com.tarterware.roadrunner.configs.SecurityConfig;
 import com.tarterware.roadrunner.services.DirectionsService;
+import com.tarterware.roadrunner.services.GeocodingService;
 
 @SpringBootTest
 class RoadrunnerApplicationTests
@@ -14,8 +18,17 @@ class RoadrunnerApplicationTests
     private DirectionsService directionsService;
     
     @MockBean
+    private GeocodingService geocodingService;
+    
+    @MockBean
     private SecurityConfig securityConfig;
 
+    @MockBean
+    private SecurityFilterChain filterChain;
+    
+    @MockBean
+    private JwtDecoder jwtDecoder;
+    
     @Test
     void contextLoads()
     {
