9d7569edd4448d3e696bb1d3d2215ba9e374b721
Steve Tarter <steve@tarterware.com>
2025-01-16 10:25:10 -0600

    Update DirectionsService to replace file-centric naming with redis-centric.
    Fix failing test due to lack of redis bean mocks.
    Add metadata for new props added to application.properties.
    Deleted unused property spring.web.allow-cors, which I now think was a hallucinatory change suggested by AI.

--- src/test/java/com/tarterware/roadrunner/RoadrunnerApplicationTests.java
+++ src/test/java/com/tarterware/roadrunner/RoadrunnerApplicationTests.java
@@ -1,11 +1,15 @@
 package com.tarterware.roadrunner;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
+import com.tarterware.roadrunner.configs.RedisConfig;
 import com.tarterware.roadrunner.configs.SecurityConfig;
 import com.tarterware.roadrunner.services.DirectionsService;
 import com.tarterware.roadrunner.services.GeocodingService;
@@ -25,6 +29,12 @@ class RoadrunnerApplicationTests
     
 	@MockitoBean
     private SecurityConfig securityConfig;
+
+	@MockitoBean
+    private LettuceConnectionFactory redisStandAloneConnectionFactory;
+
+	@MockitoBean
+	private RedisTemplate<String, Object> redisTemplateStandAlone;
 
 	@MockitoBean
     private SecurityFilterChain filterChain;
