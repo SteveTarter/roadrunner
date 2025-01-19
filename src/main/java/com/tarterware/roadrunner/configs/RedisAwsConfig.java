1112cb31828d0574e8268b98704901c95382693e
Steve Tarter <steve@tarterware.com>
2025-01-19 16:59:56 -0600

    Add new infrastructure to allow app to connect to AWS MemoryDB.
    Add an IAM token generator to allow secure access to the AWS MemoryDB.
    Add profile 'aws'that is used to activate the new AWS connection behavior.  All non-'aws' profiles will use the redis-only connector.

--- src/main/java/com/tarterware/roadrunner/configs/RedisAwsConfig.java
+++ src/main/java/com/tarterware/roadrunner/configs/RedisAwsConfig.java
@@ -1,19 +1,23 @@
 package com.tarterware.roadrunner.configs;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@Profile("!aws") // Dev and minikube use a stock redis stack, while aws uses memorydb
-public class RedisConfig
+@Profile("aws") // Only use in the "aws" profile
+public class RedisAwsConfig
 {
     @Value("${com.tarterware.redis.host}")
     private String _redisHost;
@@ -21,15 +25,26 @@ public class RedisConfig
     @Value("${com.tarterware.redis.port}")
     private int _redisPort;
 
-    @Value("${com.tarterware.redis.password}")
-    private String redisPassword;
-
     @Bean
-    LettuceConnectionFactory redisStandAloneConnectionFactory()
+    RedisConnectionFactory redisStandAloneConnectionFactory() throws Exception
     {
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(_redisHost, _redisPort);
-        configuration.setPassword(redisPassword); // Set the password here
-        return new LettuceConnectionFactory(configuration);
+        // Generate the IAM token
+        RedisIAMTokenGenerator tokenGenerator = new RedisIAMTokenGenerator();
+        String iamAuthToken = tokenGenerator.generateIAMAuthToken(_redisHost, _redisPort);
+
+        // Configure Redis client with IAM token
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(_redisHost);
+        redisConfig.setPort(_redisPort);
+        redisConfig.setPassword(iamAuthToken); // Pass the IAM token as the Redis password
+
+        // Enable SSL using LettuceClientConfiguration
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder().useSsl() // Enable SSL
+                .and().commandTimeout(Duration.ofSeconds(10)) // Optional: Configure timeout
+                .build();
+
+        // Combine Redis configuration and Lettuce client configuration
+        return new LettuceConnectionFactory(redisConfig, clientConfig);
     }
 
     @Bean
