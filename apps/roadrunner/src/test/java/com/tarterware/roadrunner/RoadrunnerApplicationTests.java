f7557fc908d6076c523a8253d183a7d813095fde
Steve Tarter <steve@tarterware.com>
2025-01-28 15:18:48 -0600

    Update application startup so that the update thread doesn't start in test.
    The UpdateThread in VehicleManager should run routinely, but wasn't because of failures in the JUnit tests.

--- apps/roadrunner/src/test/java/com/tarterware/roadrunner/RoadrunnerApplicationTests.java
+++ apps/roadrunner/src/test/java/com/tarterware/roadrunner/RoadrunnerApplicationTests.java
@@ -1,11 +1,16 @@
 package com.tarterware.roadrunner;
 
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import com.tarterware.roadrunner.configs.RedisConfig;
@@ -15,6 +20,7 @@ import com.tarterware.roadrunner.services.GeocodingService;
 import com.tarterware.roadrunner.services.IsochroneService;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RoadrunnerApplicationTests
 {
     @MockitoBean
@@ -36,13 +42,20 @@ class RoadrunnerApplicationTests
     private LettuceConnectionFactory redisStandAloneConnectionFactory;
 
     @MockitoBean
-    private RedisTemplate<String, Object> redisTemplateStandAlone;
-
-    @MockitoBean
     private SecurityFilterChain filterChain;
 
     @MockitoBean
     private JwtDecoder jwtDecoder;
+
+    @MockitoBean
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @BeforeEach
+    void setup() throws IOException
+    {
+        // Initialize mocks
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
     void contextLoads()
