ee8ec226c876724529d329811b31f64db7e99d7c
Steve Tarter <steve@tarterware.com>
2025-03-07 16:00:34 -0600

    Try to increase performance by reducing Redis traffic.
    Update getVehicleMap so that it doesn't use SSCAN to page through Vehicles.
    Add @Scheduled task to retrieve active Vehicle list periodically.  This list should be used for other scheduled tasks, and for Controller access.
    Add deletion of vehicle data when the vehicle is timed out.
    Update Javadoc for VehicleManager class.
    Add logging message when Vehicle jitter exceeds 400ms, in order to determine how and if overruns cluster.

--- src/main/java/com/tarterware/roadrunner/components/VehicleManager.java
+++ src/main/java/com/tarterware/roadrunner/components/VehicleManager.java
@@ -13,6 +13,7 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -31,9 +32,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.convert.DurationStyle;
 import org.springframework.core.env.Environment;
-import org.springframework.data.redis.core.Cursor;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -108,6 +107,8 @@ public class VehicleManager
 
     // Thread‑safe map to keep track of vehicles currently loading directions.
     private final ConcurrentMap<UUID, Future<Directions>> directionsLoadingMap = new ConcurrentHashMap<>();
+
+    private CopyOnWriteArrayList<String> activeIdsList = new CopyOnWriteArrayList<String>();
 
     // Service to retrieve directions for trip plans
     private DirectionsService directionsService;
@@ -211,47 +212,46 @@ public class VehicleManager
      *
      * @param page     page number to retrieve.
      * @param pageSize number of Vehicles in each page.
-     * @return A synchronized map of Vehicles by their UUIDs.
+     * @return A Map of Vehicles by their UUIDs.
      */
     public Map<UUID, Vehicle> getVehicleMap(int page, int pageSize)
     {
         // Create a Map of Vehicles to return
         Map<UUID, Vehicle> vMap = new HashMap<>();
 
-        // Use SSCAN to iterate over the elements of the set
-        ScanOptions scanOptions = ScanOptions.scanOptions().count(pageSize).build();
-        Cursor<Object> cursor = redisTemplate.opsForSet().scan(ACTIVE_VEHICLE_REGISTRY, scanOptions);
-
-        // Skip over records until we get to the target window.
-        int recordsToSkip = page * pageSize;
-        while ((recordsToSkip > 0) && cursor.hasNext())
+        int size = activeIdsList.size();
+        if (size == 0)
         {
-            cursor.next();
-            --recordsToSkip;
+            return vMap;
+        }
+
+        int index = page * pageSize;
+        if (index > size)
+        {
+            throw new IllegalArgumentException("Page " + page + " of page size " + pageSize + " outside bounds!");
         }
 
         // Now, loop through the cursor, filling up to "pageSize" vehicleId elements.
         int recordsToFill = pageSize;
-        List<String> vehicleIdList = new ArrayList<>();
-        while ((recordsToFill > 0) && cursor.hasNext())
+        List<String> idList = new ArrayList<>();
+        while ((recordsToFill > 0) && (index < size))
         {
-            String vehicleId = cursor.next().toString();
-            vehicleIdList.add(vehicleId);
+            String vehicleId = activeIdsList.get(index++);
+            idList.add(vehicleId);
             recordsToFill--;
         }
 
         // Convert each vehicle ID to its full key
-        List<String> keys = vehicleIdList.stream().map(id -> getVehicleKey(UUID.fromString(id)))
-                .collect(Collectors.toList());
+        List<String> keys = idList.stream().map(id -> getVehicleKey(UUID.fromString(id))).collect(Collectors.toList());
 
         // Fetch multiple Vehicle objects using opsForValue()
         List<Object> vehicleObjects = redisTemplate.opsForValue().multiGet(keys);
 
         // Process the fetched Vehicle objects
-        for (int i = 0; i < vehicleIdList.size(); i++)
+        for (int i = 0; i < idList.size(); i++)
         {
             Object obj = vehicleObjects.get(i);
-            String id = vehicleIdList.get(i);
+            String id = idList.get(i);
             if (obj == null)
             {
                 // If the vehicle is null, perform redis cleanup.
@@ -273,6 +273,23 @@ public class VehicleManager
         return vMap;
     }
 
+    /**
+     * Resets the simulation by clearing all vehicle data and related information
+     * from Redis. This method deletes the following keys and their associated
+     * values:
+     * <ul>
+     * <li>{@link #VEHICLE_UPDATE_QUEUE_ZSET}: The sorted set containing vehicles to
+     * be updated.</li>
+     * <li>{@link #ACTIVE_VEHICLE_REGISTRY}: The set of active vehicle IDs.</li>
+     * <li>{@link #TRIP_PLAN_KEY}: The key storing trip plan information.</li>
+     * <li>{@link #VEHICLE_UPDATE_LOCK_SET}: The set used for locking vehicles
+     * during updates.</li>
+     * <li>All keys starting with "vehicle:": These keys store individual vehicle
+     * data.</li>
+     * </ul>
+     * This method is typically used to clean up the Redis database before starting
+     * a new simulation or to remove all existing vehicle data.
+     */
     public void reset()
     {
         logger.info("Resetting the Redis variables");
@@ -281,6 +298,9 @@ public class VehicleManager
         redisTemplate.delete(ACTIVE_VEHICLE_REGISTRY);
         redisTemplate.delete(TRIP_PLAN_KEY);
         redisTemplate.delete(VEHICLE_UPDATE_LOCK_SET);
+
+        // Delete all of the Vehicle data.
+        redisTemplate.delete("vehicle:*");
     }
 
     /**
@@ -298,6 +318,13 @@ public class VehicleManager
         return vehicle;
     }
 
+    /**
+     * Returns the Redis key used to store a Vehicle object. The key is formatted as
+     * "vehicle:{vehicleId}".
+     *
+     * @param vehicleId The UUID of the Vehicle.
+     * @return The formatted Redis key for the Vehicle.
+     */
     public String getVehicleKey(UUID vehicleId)
     {
         return String.format("vehicle:%s", vehicleId.toString());
@@ -347,6 +374,25 @@ public class VehicleManager
         return vehicle;
     }
 
+    /**
+     * Processes a TripPlan to generate route geometry and line segment data for a
+     * Vehicle. This method performs the following steps: *
+     * <ol>
+     * <li>Fetches directions for the trip plan using the DirectionsService.</li>
+     * <li>Initializes coordinate transformers for converting between WGS84 and UTM
+     * coordinates.</li>
+     * <li>Iterates over route legs and steps to extract coordinate data.</li>
+     * <li>Converts WGS84 coordinates to UTM coordinates and handles transitions
+     * between UTM zones.</li>
+     * <li>Generates line segments for each UTM zone and stores them as
+     * LineSegmentData objects.</li>
+     * <li>Stores the Directions and LineSegmentData in the respective maps for
+     * later use.</li>
+     * </ol>
+     * 
+     * @param vehicleId The UUID of the Vehicle.
+     * @param tripPlan  The TripPlan containing route and stop information.
+     */
     public void processTripPlanData(UUID vehicleId, TripPlan tripPlan)
     {
         // Check for a valid trip plan.
@@ -445,7 +491,9 @@ public class VehicleManager
     /**
      * Retrieves the Directions associated with a Vehicle.
      *
-     * @param vehicleId The UUID of the Vehicle.
+     * @param vehicleId     The UUID of the Vehicle.
+     * @param waitForResult True if the caller wants to wait for the result of the
+     *                      DirectionsService call; false otherwise.
      * @return The Directions object for the Vehicle.
      */
     public Directions getVehicleDirections(UUID vehicleId, boolean waitForResult)
@@ -513,6 +561,25 @@ public class VehicleManager
 
     long lastNsManagerStartTime = System.nanoTime();
 
+    /**
+     * Periodically updates the positions of all active vehicles. This method
+     * retrieves a set of vehicles that are ready to be updated based on their last
+     * update time and the configured update period. For each vehicle, it performs
+     * the following steps: *
+     * <ol>
+     * <li>Retrieves the vehicle's directions and line segment data.</li>
+     * <li>Calculates the time since the last update and updates the vehicle's
+     * position if necessary.</li>
+     * <li>Records jitter statistics, which measure the deviation from the expected
+     * update interval.</li>
+     * <li>Updates the vehicle's state in Redis, including its last execution time
+     * and the manager host that performed the update.</li>
+     * <li>Checks if the vehicle has timed out and marks it for deletion if
+     * necessary.</li>
+     * </ol>
+     * After processing all vehicles, the method cleans up deleted vehicles and
+     * updates the jitter statistics for monitoring performance.
+     */
     @Scheduled(fixedRateString = "${com.tarterware.roadrunner.vehicle-polling-period}")
     public void updateVehicles()
     {
@@ -526,6 +593,7 @@ public class VehicleManager
         int readyVehicleCount = 0;
         if (readyVehicles != null)
         {
+            int index = 0;
             readyVehicleCount = readyVehicles.size();
 
             for (Object vehicleIdObj : readyVehicles)
@@ -571,6 +639,11 @@ public class VehicleManager
                                 }
                                 statisticsCollector.recordMeasurement(msJitter);
 
+                                if (msJitter > 400)
+                                {
+                                    logger.info("{}/{}; {}; Vehicle {}", index, readyVehicleCount, vehicleId, msJitter);
+                                }
+
                                 // Update vehicle execution time and store vehicle state
                                 vehicle.setLastNsExecutionTime(System.nanoTime() - nsVehicleStartTime);
 
@@ -590,7 +663,6 @@ public class VehicleManager
                                         logger.info("Deleting vehicle ID {}", vehicle.getId());
                                     }
                                 }
-
                             }
                         }
                         catch (Exception e)
@@ -603,20 +675,19 @@ public class VehicleManager
                         }
                     }
                 }
+                index++;
             }
         }
+
+        // Clean up the data of the vehicles that have been found to be deleted
+        cleanupDeletedVehicles(deletionSet);
 
         // Take care of corner case: with no vehicles, the stats never update.
         // Add some jitter-free numbers so the averages will trend down.
         if (vehicleCount == 0)
         {
             statisticsCollector.recordMeasurement(0);
-            statisticsCollector.recordMeasurement(0);
-            statisticsCollector.recordMeasurement(0);
-            statisticsCollector.recordMeasurement(0);
-            statisticsCollector.recordMeasurement(0);
         }
-        cleanupDeletedVehicles(deletionSet);
 
         // Update the statistics endpoint variables.
         double msMinimumJitterTime = statisticsCollector.getMin();
@@ -639,12 +710,33 @@ public class VehicleManager
                 .log();
     }
 
+    /**
+     * Fetches a set of vehicles that are ready to be updated based on the current
+     * time and the configured update period.
+     *
+     * @param currentEpochMillis The current time in milliseconds since the epoch.
+     * @return A set of vehicle IDs (as Objects) that are ready for update.
+     */
     private Set<Object> fetchReadyVehicles(long currentEpochMillis)
     {
         return redisTemplate.opsForZSet().rangeByScore(VEHICLE_UPDATE_QUEUE_ZSET, 0,
                 currentEpochMillis - msUpdatePeriod + msPollingPeriod);
     }
 
+    /**
+     * Cleans up deleted vehicles by removing their data from Redis and local
+     * caches. This method performs the following actions: *
+     * <ul>
+     * <li>Removes the vehicle IDs from the {@link #ACTIVE_VEHICLE_REGISTRY}
+     * set.</li>
+     * <li>Removes the vehicle's directions and line segment data from the local
+     * maps.</li>
+     * <li>Removes the vehicle's state from Redis, including its entry in the
+     * {@link #VEHICLE_UPDATE_QUEUE_ZSET} and the {@link #ACTIVE_VEHICLE_REGISTRY}
+     * set.</li>
+     * </ul>
+     * * @param deletionSet The set of vehicle IDs to be deleted.
+     */
     private void cleanupDeletedVehicles(Set<UUID> deletionSet)
     {
         // Remove from Active Vehicle Registry
@@ -664,22 +756,30 @@ public class VehicleManager
             String hashKey = vehicleID.toString();
             redisTemplate.opsForZSet().remove(VEHICLE_UPDATE_QUEUE_ZSET, hashKey);
             redisTemplate.opsForSet().remove(ACTIVE_VEHICLE_REGISTRY, hashKey);
+
+            redisTemplate.delete(getVehicleKey(vehicleID));
         });
     }
 
+    /**
+     * Periodically reconciles the local caches ({@link #directionsMap} and
+     * {@link #lineSegmentDataMap}) with the active vehicle list in Redis. This
+     * method ensures that the caches do not contain data for vehicles that have
+     * been removed from the active vehicle registry. It also updates the
+     * {@link #statisticsCollector} to ensure that it has enough capacity to store
+     * jitter statistics for the current number of active vehicles.
+     */
     @Scheduled(fixedRate = 60000) // every minute
     public void reconcileLocalCache()
     {
-        // Get the set of active vehicle IDs from Redis
-        Set<Object> activeIds = redisTemplate.opsForSet().members(ACTIVE_VEHICLE_REGISTRY);
-
         // For directionsMap, create a list of IDs to remove
         List<UUID> removedFromDirections = new ArrayList<>();
+
         // Iterate over a copy of the key set to avoid ConcurrentModificationException
         Set<UUID> directionsKeys = new HashSet<>(directionsMap.keySet());
         for (UUID vehicleId : directionsKeys)
         {
-            if (!activeIds.contains(vehicleId.toString()))
+            if (!activeIdsList.contains(vehicleId.toString()))
             {
                 removedFromDirections.add(vehicleId);
                 directionsMap.remove(vehicleId);
@@ -695,7 +795,7 @@ public class VehicleManager
         Set<UUID> lineSegmentKeys = new HashSet<>(lineSegmentDataMap.keySet());
         for (UUID vehicleId : lineSegmentKeys)
         {
-            if (!activeIds.contains(vehicleId.toString()))
+            if (!activeIdsList.contains(vehicleId.toString()))
             {
                 removedFromLineSegmentData.add(vehicleId);
                 lineSegmentDataMap.remove(vehicleId);
@@ -706,12 +806,12 @@ public class VehicleManager
             logger.info("Reconciled lineSegmentDataMap: Removed vehicles: {}", removedFromLineSegmentData);
         }
 
-        // Update the JitterStatisticsCollector so that the 10 last stats will be
-        // recorded.
+        // Update the JitterStatisticsCollector so that the 10 last statistics for each
+        // vehicle will be recorded.
         int vehicleCount = (int) (10 * redisTemplate.opsForSet().size(ACTIVE_VEHICLE_REGISTRY));
-        if (vehicleCount < 200)
+        if (vehicleCount < 10)
         {
-            vehicleCount = 200;
+            vehicleCount = 10;
         }
 
         if (vehicleCount != statisticsCollector.getCount())
@@ -720,5 +820,28 @@ public class VehicleManager
 
             statisticsCollector = new JitterStatisticsCollector(statisticsCollector, vehicleCount);
         }
+    }
+
+    /**
+     * Periodically retrieves the current list of active vehicles from Redis and
+     * updates the local cache. This method fetches the members of the
+     * {@link #ACTIVE_VEHICLE_REGISTRY} set from Redis, converts them to a list of
+     * strings, and updates the {@link #activeIdsList} with the new list. The
+     * {@link #activeIdsList} is a thread-safe list that provides a snapshot of the
+     * active vehicles at a specific point in time, allowing other methods to safely
+     * iterate over the list without encountering ConcurrentModificationExceptions.
+     */
+    @Scheduled(fixedRate = 1000) // every second
+    public void getCurrentVehicleList()
+    {
+        // Grab the members of the active Vehicle Set and convert it to a Java List
+        Set<Object> idSet = redisTemplate.opsForSet().members(ACTIVE_VEHICLE_REGISTRY);
+        List<String> idsList = idSet.stream().map(Object::toString).collect(Collectors.toList());
+
+        // Update the shared instance variable. Note that CopyOnWriteArrayList,
+        // iterators operate on a snapshot of the list at the time the iterator was
+        // created, so the other threads shouldn't throw exceptions.
+        activeIdsList.clear();
+        activeIdsList.addAll(idsList);
     }
 }
