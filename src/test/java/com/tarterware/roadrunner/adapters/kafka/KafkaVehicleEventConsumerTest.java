f98dfa0d55657b126f42acea693bdc681ec670c5
Steve Tarter <steve@tarterware.com>
2026-04-09 16:35:07 -0500

    Update consumer test so that it will run without Redis running.

--- src/test/java/com/tarterware/roadrunner/adapters/kafka/KafkaVehicleEventConsumerTest.java
+++ src/test/java/com/tarterware/roadrunner/adapters/kafka/KafkaVehicleEventConsumerTest.java
@@ -9,11 +9,25 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import com.tarterware.roadrunner.configs.NoOpSchedulerConfig;
+import com.tarterware.roadrunner.configs.RedisConfig;
+import com.tarterware.roadrunner.configs.SecurityConfig;
 import com.tarterware.roadrunner.messaging.VehiclePositionEvent;
 import com.tarterware.roadrunner.models.VehicleState;
 import com.tarterware.roadrunner.ports.ControllerVehicleStateStore;
+import com.tarterware.roadrunner.services.DirectionsService;
+import com.tarterware.roadrunner.services.GeocodingService;
+import com.tarterware.roadrunner.services.IsochroneService;
+
+import io.fabric8.kubernetes.client.KubernetesClient;
 
 @SpringBootTest(
         properties =
@@ -24,6 +38,42 @@ import com.tarterware.roadrunner.ports.ControllerVehicleStateStore;
 @Import(NoOpSchedulerConfig.class)
 public class KafkaVehicleEventConsumerTest
 {
+    @MockitoBean
+    private DirectionsService directionsService;
+
+    @MockitoBean
+    private GeocodingService geocodingService;
+
+    @MockitoBean
+    private IsochroneService isochroneService;
+
+    @MockitoBean
+    private SecurityConfig securityConfig;
+
+    @MockitoBean
+    private RedisConfig redisConfig;
+
+    @MockitoBean
+    private LettuceConnectionFactory redisStandAloneConnectionFactory;
+
+    @MockitoBean
+    private SecurityFilterChain filterChain;
+
+    @MockitoBean
+    private JwtDecoder jwtDecoder;
+
+    @MockitoBean
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @MockitoBean
+    private KubernetesClient kubernetesClient;
+
+    @MockitoBean
+    private KafkaTemplate<String, VehiclePositionEvent> kafkaTemplate;
+
+    @MockitoBean
+    private SetOperations<String, Object> mockSetOperations;
+
     @Autowired
     private ControllerVehicleStateStore stateStore;
 
